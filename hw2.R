b66c56f3340350bc90d69cd4d25f6768a4193067
Billcyx <yuxi_chen0815@sina.com>
2024-02-29 08:22:11 -0500

    Add files via upload

--- hw2.R
+++ hw2.R
@@ -1,4 +1,15 @@
 library("readxl")
 
-data <- read_excel("/Users/yuxichen/biostat620/hw1/ScreenTime_PSong.xlsx")
+data <- read_excel("/Users/yuxichen/biostat620/hw2/yuxi_data.xlsx")
 
+###Seemingly unrelated regression (SUR) 
+library(systemfit)
+
+model1 = Total.ST.min ~ total_lag1 + Xt + Zt
+model2 = Social.ST.min ~ social_lag1 + Xt + Zt
+
+model = list(model1, model2)
+
+sur_model = systemfit(model, method="SUR", data = data)
+
+summary(sur_model)
